df58852b9d4ca1c65853362a6d82450825dab912
Barnich-Kate <85077866+Barnich-Kate@users.noreply.github.com>
2023-05-29 21:06:12 +0300

    Create task18.cpp
    Add a solution of the task number 18

---
+++ task18.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+		
+	int A, B, C, AC, BC;
+	cout << "Type the value of A";
+	cin >> A;
+	cout << "Type the value of B";
+	cin >> B;
+	cout << "Type the value of C";
+	cin >> C;
+	AC = A*C;
+	BC = B*C;
+	cout << "The lenth of the segment AC is equal to" << AC << endl;
+	cout << "The lenth of the segment BC is equal to" << BC << endl;
+	return 0;
+
+
+}
